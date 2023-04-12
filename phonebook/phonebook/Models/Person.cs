065033e52824c8aa82cdd5c6d99a62247db26847
NecatiKumdereli <21660110053@nevsehir.edu.tr>
2023-04-12 14:29:45 +0300

    Mvc-phonebook

---
+++ phonebook/phonebook/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace phonebook.Models;
+
+public partial class Person
+{
+    public long PersonId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public long Number { get; set; }
+}
---
+++ phonebook/phonebook/Models/PhoneBookContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace phonebook.Models;
+
+public partial class PhoneBookContext : DbContext
+{
+    public PhoneBookContext()
+    {
+    }
+
+    public PhoneBookContext(DbContextOptions<PhoneBookContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=.\\Data\\PhoneBook.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.ToTable("Person");
+
+            entity.Property(e => e.PersonId).HasColumnName("PersonID");
+            entity.Property(e => e.LastName).HasColumnName("Last Name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
